7b9c6858a566d06c9fa894483c7ab1362f59e3b1
Egor <egordusik@gmail.com>
2021-12-16 23:14:18 +0300

    Composable State

--- app/src/main/java/com/example/composeexp/MainActivity.kt
+++ app/src/main/java/com/example/composeexp/MainActivity.kt
@@ -4,61 +4,60 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.sp
-
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fontFamily = FontFamily(
-            Font(R.font.roboto_light, FontWeight.Light),
-            Font(R.font.roboto_medium, FontWeight.Medium),
-            Font(R.font.roboto_black, FontWeight.Black),
-            Font(R.font.roboto_black_italic, FontWeight.Black, FontStyle.Italic),
-        )
+
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black)
-            ) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Blue,
-                                fontSize = 40.sp,
-                                fontStyle = FontStyle.Italic,
-                                fontWeight = FontWeight.Medium
-                            )
-                        ) {
-                            append("Simple ")
-                        }
-                        append("String")
-                    },
-                    color = Color.White,
-                    fontSize = 32.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Light,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.Underline
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+
+                ) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .background(color.value)
                 )
             }
         }
     }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
+    Box(modifier = modifier
+        .background(Color.Blue)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        })
 }
