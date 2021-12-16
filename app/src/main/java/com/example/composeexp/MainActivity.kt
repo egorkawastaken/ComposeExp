1a80f92cf5eb2dc4b78103b1d5f07810bf305088
Egor <egordusik@gmail.com>
2021-12-16 20:50:40 +0300

    Simple Text Styling

--- app/src/main/java/com/example/composeexp/MainActivity.kt
+++ app/src/main/java/com/example/composeexp/MainActivity.kt
@@ -3,87 +3,62 @@ package com.example.composeexp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.roboto_light, FontWeight.Light),
+            Font(R.font.roboto_medium, FontWeight.Medium),
+            Font(R.font.roboto_black, FontWeight.Black),
+            Font(R.font.roboto_black_italic, FontWeight.Black, FontStyle.Italic),
+        )
         setContent {
-            val painter = painterResource(id = R.drawable.sad_cat)
-            val title = "Sad Cat"
-            val description = "This is a really sad cat"
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
-            ) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title)
-            }
-
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(16.dp),
-        elevation = 8.dp
-    ) {
-        Box(
-            modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    ) )
-            Text(
-                text = title,
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(12.dp),
-                fontSize = 16.sp,
-                style = TextStyle(Color.White)
-            )
+                    .background(Color.Black)
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Blue,
+                                fontSize = 40.sp,
+                                fontStyle = FontStyle.Italic,
+                                fontWeight = FontWeight.Medium
+                            )
+                        ) {
+                            append("Simple ")
+                        }
+                        append("String")
+                    },
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Light,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
             }
-
+        }
     }
 }
